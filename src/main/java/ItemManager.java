9c7a5cf262dac2249e2902499e1f248a3d3a21a0
goldragoon <paganinist@gmail.com>
2016-09-07 19:57:22 +0900

    Fix #5 Add reset button on bottom panel which rollback current item attribute value changes. Add fool-proof function when change selected item. If current changes are not applied to result file, then the program will show off the alert dialog. Add Save Function. Drastic changes in the way of storing attributes. (more elegant way)

--- src/main/java/ItemManager.java
+++ src/main/java/ItemManager.java
@@ -2,11 +2,9 @@ import java.io.*;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-/**
- * Created by Lab_kairos on 2016-08-27.
- */
 public class ItemManager {
     List<ItemInfo> items;
 
@@ -31,11 +29,10 @@ public class ItemManager {
                 }
                 items.add(parseItem(line));
             }
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
+        catch (URISyntaxException e) {e.printStackTrace();}
+        catch (IOException e) {e.printStackTrace();}
+        catch (NullPointerException e) {e.printStackTrace();}
 
         this.items = items;
     }
@@ -47,8 +44,8 @@ public class ItemManager {
             OutputStreamWriter osw = new OutputStreamWriter(fos,"euc-kr");
             BufferedWriter bufferedWriter = new BufferedWriter(osw);
 
-            for(int i=0;i<items.size();i++) {
-                bufferedWriter.write(getItemString(items.get(i)));
+            for(ItemInfo item:items) {
+                bufferedWriter.write(getItemString(item));
                 bufferedWriter.newLine();
             }
             bufferedWriter.close();
@@ -62,116 +59,15 @@ public class ItemManager {
 
     private String getItemString(ItemInfo item) {
         StringBuffer sb = new StringBuffer();
-        sb.append(item.getNo()+",");
-        sb.append(item.getName()+",");
-        sb.append(item.getType()+",");
-        sb.append(item.getOption()+",");
-        sb.append(item.getWearingImage()+",");
-        sb.append(item.getWearingImageDyeing()+",");
-        sb.append(item.getImage()+",");
-        sb.append(item.getImageDyeing()+",");
-        sb.append(item.getMaxNumber()+",");
-        sb.append(item.getDurability()+",");
-        sb.append(item.getCost()+",");
-        sb.append(item.getFixCost()+",");
-        sb.append(item.getNamingCost()+",");
-        sb.append(item.getPowerRequirement()+",");
-        sb.append(item.getAfilityRequirement()+",");
-        sb.append(item.getIntellictRequirement()+",");
-        sb.append(item.getGenderRestriction()+",");
-        sb.append(item.getJobRestriction()+":");
-        sb.append(item.getAdvancementRestriction()+",");
-        sb.append(item.getLevelRestriction()+",");
-        sb.append(item.getAccuracy()+",");
-        sb.append(item.getDamage()+",");
-        sb.append(item.getPower()+",");
-        sb.append(item.getAfility()+",");
-        sb.append(item.getIntellict()+",");
-        sb.append(item.getHealth()+",");
-        sb.append(item.getMagic()+",");
-        sb.append(item.getHealthPercentage()+",");
-        sb.append(item.getMagicPercentage()+",");
-        sb.append(item.getArmed()+",");
-        sb.append(item.getMagicDefencePower() +",");
-        sb.append(item.getRegeneration() + ",");
-        sb.append(item.getsMinDamage()+",");
-        sb.append(item.getsMaxDamage()+",");
-        sb.append(item.getiMinDamage()+",");
-        sb.append(item.getiMaxDamage()+",");
-        sb.append(item.getWieldingSound()+",");
-        sb.append(item.getHitSound()+",");
-        sb.append(item.getUnit()+",");
-        sb.append(item.getScript()+",");
-        sb.append(item.getScriptProbability()+",");
-        sb.append(item.getWearingScript()+",");
-        sb.append(item.getWearOffScript()+",");
+        for(String value: item.values) sb.append(value+",");
         return sb.toString();
     }
 
     public List<ItemInfo> findItemByName(String itemName) {
-
         List<ItemInfo> foundItems = new ArrayList<ItemInfo>();
-        for(int i=0; i<items.size(); i++) {
-            if(items.get(i).getName().contains(itemName)) {
-                foundItems.add(items.get(i));
-            }
-        }
         return foundItems;
     }
 
-    private ItemInfo parseItem(String itemString) {
-        ItemInfo itemInfo = new ItemInfo();
-        String[] item = itemString.split(",");
-        itemInfo.setNo(Integer.parseInt(item[0]));
-        itemInfo.setName(item[1]);
-        itemInfo.setType(Integer.parseInt(item[2]));
-        itemInfo.setOption(Integer.parseInt(item[3]));
-        itemInfo.setWearingImage(Integer.parseInt(item[4]));
-        itemInfo.setWearingImageDyeing(Integer.parseInt(item[5]));
-        itemInfo.setImage(Integer.parseInt(item[6]));
-        itemInfo.setImageDyeing(Integer.parseInt(item[7]));
-        itemInfo.setMaxNumber(Integer.parseInt(item[8]));
-        itemInfo.setDurability(Integer.parseInt(item[9]));
-        itemInfo.setCost(Integer.parseInt(item[10]));
-        itemInfo.setFixCost(Integer.parseInt(item[11]));
-        itemInfo.setNamingCost(Integer.parseInt(item[12]));
-        itemInfo.setPowerRequirement(Integer.parseInt(item[13]));
-        itemInfo.setAfilityRequirement(Integer.parseInt(item[14]));
-        itemInfo.setIntellictRequirement(Integer.parseInt(item[15]));
-        itemInfo.setGenderRestriction(Integer.parseInt(item[16]));
-        itemInfo.setJobRestriction(Integer.parseInt(item[17].split(":")[0]));
-        itemInfo.setAdvancementRestriction(Integer.parseInt(item[17].split(":")[1]));
-        itemInfo.setLevelRestriction(Integer.parseInt(item[18]));
-        itemInfo.setAccuracy(Integer.parseInt(item[19]));
-        itemInfo.setDamage(Integer.parseInt(item[20]));
-        itemInfo.setPower(Integer.parseInt(item[21]));
-        itemInfo.setAfility(Integer.parseInt(item[22]));
-        itemInfo.setIntellict(Integer.parseInt(item[23]));
-        itemInfo.setHealth(Integer.parseInt(item[24]));
-        itemInfo.setMagic(Integer.parseInt(item[25]));
-        itemInfo.setHealthPercentage(Integer.parseInt(item[26]));
-        itemInfo.setMagicPercentage(Integer.parseInt(item[27]));
-        itemInfo.setArmed(Integer.parseInt(item[28]));
-        itemInfo.setMagicDefencePower(Integer.parseInt(item[29]));
-        itemInfo.setRegeneration(Integer.parseInt(item[30]));
-        itemInfo.setsMinDamage(Integer.parseInt(item[31]));
-        itemInfo.setsMaxDamage(Integer.parseInt(item[32]));
-        itemInfo.setiMinDamage(Integer.parseInt(item[33]));
-        itemInfo.setiMaxDamage(Integer.parseInt(item[34]));
-        itemInfo.setWieldingSound(Integer.parseInt(item[35]));
-        itemInfo.setHitSound(Integer.parseInt(item[36]));
-        itemInfo.setUnit(item[37]);
-        itemInfo.setScript(item[38]);
-        try {
-            itemInfo.setScriptProbability(Integer.parseInt(item[39]));
-            itemInfo.setWearingScript(item[40]);
-            itemInfo.setWearOffScript(item[41]);
-        } catch (ArrayIndexOutOfBoundsException e) {
-            itemInfo.setWearingScript("");
-            itemInfo.setWearOffScript("");
-        }
-
-        return itemInfo;
-    }
+    private ItemInfo parseItem(String itemString) {return new ItemInfo(new ArrayList<String>(Arrays.asList(itemString.split(","))));}
 
 }
--- src/main/java/MainFrame.java
+++ src/main/java/MainFrame.java
@@ -1,7 +1,15 @@
+import com.sun.istack.internal.Nullable;
+
 import javax.swing.*;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.io.*;
 import java.lang.reflect.Method;
 import java.net.URISyntaxException;
@@ -12,21 +20,22 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Callable;
 
-/**
- * Created by Lab_kairos on 2016-08-28.
- */
-public class MainFrame implements ListSelectionListener{
-    ItemManager im  = new ItemManager();
-    List<String> itemNames = new ArrayList<String>();
-    JFrame frame = new JFrame("Item Manager - by kairos & paganinist");
-    JPanel topPanel = new JPanel();
-    JPanel searchListPanel = new JPanel();
-    JPanel attributePanel = new JPanel();
-    JPanel bottomPanel = new JPanel();
-    JLabel titleLabel = new JLabel();
-    JList list = new JList();
-    Map<String,Component> components = new HashMap<String, Component>();
+public class MainFrame implements ListSelectionListener, KeyEventDispatcher{
+    private ItemManager im  = new ItemManager();
+    private List<String> itemNames = new ArrayList<String>();
+    private JFrame frame = new JFrame("Item Manager - by kairos & paganinist");
+    private JPanel topPanel = new JPanel();
+    private JPanel searchListPanel = new JPanel();
+    private JPanel attributePanel = new JPanel();
+    private JPanel bottomPanel = new JPanel();
+    private JLabel titleLabel = new JLabel();
+    private JList list = new JList();
 
+    private JTextField attFields[] = new JTextField[ItemInfo.attributes_size];
+    private int currentAmending = 0;
+    private Map<String,Component> components = new HashMap<String, Component>();
+    private boolean controlKeyPressed = false, shiftKeyPressed = false;
+    private boolean contextSaved = true; // does last attribute amendation saved
     private void createTopPanel() {
         JPanel searchPanel = new JPanel();
         JPanel titlePanel = new JPanel();
@@ -42,25 +51,22 @@ public class MainFrame implements ListSelectionListener{
         topPanel.add(searchPanel);
         topPanel.add(titlePanel);
     }
-
     private void createBottomPanel() {
         JPanel saveLoadPanel = new JPanel();
-        JButton loadButton = new JButton("불러오기");
         JButton saveButton = new JButton("저장");
-
-        components.put("loadButton",loadButton);
+        JButton resetButton = new JButton("리셋");
+        saveButton.addActionListener(new ActionListener() {public void actionPerformed(ActionEvent e) {save();}});
+        resetButton.addActionListener(new ActionListener() {public void actionPerformed(ActionEvent e) {reset();}});
         components.put("saveButton", saveButton);
-
         saveLoadPanel.add(saveButton);
-        saveLoadPanel.add(loadButton);
-
+        saveLoadPanel.add(resetButton);
         bottomPanel.setLayout(new GridLayout(1,2));
         bottomPanel.add(new JPanel());
         bottomPanel.add(saveLoadPanel);
     }
     private void createSearchListPanel(List<ItemInfo> itemList) {
         for(ItemInfo i : itemList){
-            itemNames.add(i.getName());
+            itemNames.add(i.getAttribute(ItemInfo.Attributes.ITEM_NAME));
         }
 
         JScrollPane scrollPane = new JScrollPane();
@@ -83,31 +89,38 @@ public class MainFrame implements ListSelectionListener{
         URL fileUrl = getClass().getClassLoader().getResource("attribute_list.txt");
 
         try {
-            String line;
+
             File file = new File(fileUrl.toURI());
             FileInputStream fis = new FileInputStream(file);
             BufferedReader bis = new BufferedReader(new InputStreamReader(fis));
-            while((line = bis.readLine()) != null)
+            for(int l = 0; l < ItemInfo.attributes_size; l++)
             {
-                System.out.println(line);
                 JPanel attPanel = new JPanel();
-                JLabel attLabel = new JLabel(line + " : ");
-                JTextField attField = new JTextField("",20);
-                attField.setText("");
+                JLabel attLabel = new JLabel(bis.readLine() + " : ");
+                attFields[l] = new JTextField("",20);
+                attFields[l].getDocument().addDocumentListener(new DocumentListener() {
+                    public void insertUpdate(DocumentEvent e) {
+                        contextSaved = false;
+                        titleLabel.setForeground(Color.BLUE);
+                    }
+
+                    public void removeUpdate(DocumentEvent e) {
+                        contextSaved = false;
+                        titleLabel.setForeground(Color.BLUE);
+                    }
+
+                    public void changedUpdate(DocumentEvent e) {
+
+                    }
+                });
                 attPanel.add(attLabel);
-                attPanel.add(attField);
+                attPanel.add(attFields[l]);
                 scrollPane_inner.add(attPanel);
             }
-
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }catch(IOException e)
-        {
-            e.printStackTrace();
-        }catch(URISyntaxException e)
-        {
-            e.printStackTrace();
         }
+        catch (FileNotFoundException e) {e.printStackTrace();}
+        catch(IOException e) {e.printStackTrace();}
+        catch(URISyntaxException e) {e.printStackTrace();}
 
         scrollPane.setMinimumSize(new Dimension(290, 510));
         scrollPane.setPreferredSize(new Dimension(290, 510));
@@ -116,7 +129,7 @@ public class MainFrame implements ListSelectionListener{
     }
 
 
-    public void createFrame() {
+    private void createFrame() {
         im.readItemsFile();
 
         createTopPanel();
@@ -134,20 +147,105 @@ public class MainFrame implements ListSelectionListener{
 
         frame.setSize(600,610);
         frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        KeyboardFocusManager kfm = KeyboardFocusManager.getCurrentKeyboardFocusManager();
+        kfm.addKeyEventDispatcher(this);
 
     }
 
     public static void main(String[] args){
         MainFrame mainFrame = new MainFrame();
         mainFrame.createFrame();
+
     }
 
     public void valueChanged(ListSelectionEvent e) {
-        if(!e.getValueIsAdjusting()) {
-            System.out.println(e.getFirstIndex());
-            titleLabel.setText(list.getSelectedValue().toString());
+
+        final int YES = 0;
+        final int NO = 1;
+        final int CANCLE = 2;
+        int answer = 1;
+        ItemInfo ii;
+        if(!contextSaved)
+            answer = JOptionPane.showConfirmDialog(null, "저장되지 않은 변경사항이 있습니다. 어떻게 하시겠습니까?");
+
+        switch(answer) {
+            case YES:
+                save();
+            case NO:
+
+
+                ii = im.getItmes().get(list.getSelectedIndex());
+                for (int l = 0; l < ItemInfo.attributes_size; l++)
+                    attFields[l].setText(ii.getAttribute(l));
+
+                contextSaved = true;
+                titleLabel.setText(list.getSelectedValue().toString());
+                titleLabel.setForeground(Color.BLACK);
+                currentAmending = list.getSelectedIndex();
+                break;
+            case CANCLE:
+
+                break;
+
         }
+
+
     }
 
+    public boolean dispatchKeyEvent(KeyEvent e) {
+        if (e.getID() == KeyEvent.KEY_PRESSED) {
+            switch(e.getKeyCode())
+            {
+                case KeyEvent.VK_CONTROL:
+                    controlKeyPressed = true;
+                    break;
+                case KeyEvent.VK_SHIFT:
+                    shiftKeyPressed = true;
+            }
+
+            if(controlKeyPressed)
+            {
+                switch(e.getKeyCode())
+                {
+                    case KeyEvent.VK_S:
+                        save();
+                        break;
+                }
+
+            }
+        } else if (e.getID() == KeyEvent.KEY_RELEASED) {
+            switch(e.getKeyCode())
+            {
+                case KeyEvent.VK_CONTROL:
+                    controlKeyPressed = false;
+                    break;
+                case KeyEvent.VK_SHIFT:
+                    shiftKeyPressed = false;
+            }
+        }
+        return false;
+    }
+
+    private void save()
+    {
+        contextSaved = true;
+        System.out.println("Save");
+        ItemInfo target = im.getItmes().get(currentAmending);
+        for(int l = 0; l < ItemInfo.attributes_size; l++)
+            target.setAttribute(l, attFields[l].getText());
+        im.getItmes().set(currentAmending, target);
+        im.saveFile();
+        titleLabel.setForeground(Color.BLACK);
+    }
+
+    private void reset()
+    {
+        contextSaved = true;
+        System.out.println("Reset");
+        ItemInfo target = im.getItmes().get(currentAmending);
+        for(int l = 0; l < ItemInfo.attributes_size; l++)
+            attFields[l].setText(target.getAttribute(l));
+        titleLabel.setForeground(Color.BLACK);
+    }
 }
